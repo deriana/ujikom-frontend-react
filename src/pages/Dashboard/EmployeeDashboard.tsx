273c28efd7c57cb1f62d03f1ce4e2e06982a3efd
Hideri <derana765@gmail.com>
2026-03-16 16:21:29 +0700

    add get started

--- src/pages/Dashboard/EmployeeDashboard.tsx
+++ src/pages/Dashboard/EmployeeDashboard.tsx
@@ -11,6 +11,7 @@ import EmployeeDashboardSkeleton from "@/components/skeleton/EmployeeDashboardSk
 import { useEmployeeDashboard } from "@/hooks/useDashboard";
 import { useNotificationPermission } from "@/hooks/useNotificationPermission";
 import { Calendar, Clock, Coffee, Timer, LayoutDashboard } from "lucide-react";
+import { GetStarted } from "../GetStarted/Index";
 
 export default function EmployeeDashboard() {
   const currentMonth = new Intl.DateTimeFormat("en-US", {
@@ -48,6 +49,8 @@ export default function EmployeeDashboard() {
   return (
     <>
       <PageMeta title="Employee Dashboard" />
+
+      <GetStarted />
 
       {/* Header section biar gak sepi */}
       <div className="mb-6 flex flex-col gap-2 sm:flex-row sm:items-center sm:justify-between">
